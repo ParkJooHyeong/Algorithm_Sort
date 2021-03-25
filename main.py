a37b479d570a3659806690c283484354151e16b9
ParkJooHyeong <parkjh0856@gmail.com>
2021-03-25 10:37:34 +0900

    Sequential sort 구현

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import sorting as fsort
 input_list=[10,9,4,5,1,2,8,3,7,6]
 
 
+print("순차정렬 :",fsort.sequential_sort(input_list, reverse=False))
--- sorting.py
+++ sorting.py
@@ -1,28 +1,38 @@
-def sequential_sort():
+def sequential_sort(input_list, reverse):
+
+    if reverse is True:
+        for i in range(len(input_list)-1):
+            for j in range(i+1,len(input_list)):
+                if input_list[i]<input_list[j]:
+                    input_list[i], input_list[j]=input_list[j],input_list[i];
+    else:
+        for i in range(len(input_list)-1):
+            for j in range(i+1,len(input_list)):
+                if input_list[i]>input_list[j]:
+                    input_list[i], input_list[j]=input_list[j],input_list[i];
+
+    return input_list
+
+
+def quick_sort(input_list):
     answer=[]
 
     return answer
 
 
-def quick_sort():
+def binary_sort(input_list):
     answer=[]
 
     return answer
 
 
-def binary_sort():
+def counting_sort(input_list):
     answer=[]
 
     return answer
 
 
-def counting_sort():
-    answer=[]
-
-    return answer
-
-
-def heap_sort():
+def heap_sort(input_list):
     answer=[]
 
     return answer
