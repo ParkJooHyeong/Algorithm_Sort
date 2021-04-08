6bea88c539ddae030187b7643b97229971f19169
ParkJooHyeong <parkjh0856@gmail.com>
2021-04-08 10:39:58 +0900

    Add quick sort, counting sort Remark README.md

--- README.md
+++ README.md
@@ -1,20 +1,42 @@
 # Sorting Algorithm
 여러가지 sorting 알고리즘을 직접 구현 및 연습.  
+> reverse=False : 오름차순 정렬  
+> reverse=True  : 내림차순 정렬  
+>
 
 ## 1. Selection Sort  
 - 선택 정렬
+- 시간 복잡도 : O(N^2)
+    > N = 데이터의 개수
 
 ## 2. Insertion Sort  
 - 삽입 정렬
+- 시간 복잡도 : O(N^2)
+    > N = 데이터의 개수
 
 ## 3. Quick Sort
 - 퀵 정렬
+- 시간 복잡도 : O(NlogN)
+    > N = 데이터의 개
 
 ## 4. Counting Sort
 - 계수 정렬
+- 데이터의 크기 범위가 제한되어 정수 형태로 표현할 수 있을 때 사용가능.
+    > ex) 0이상 100이하의 성적 데이터를 정렬할 때.
+- 실수형 범위의 경우 무한한 범위를 가져 사용하기 어렵다.
+- 시간 복잡도 : O(N+K) 
+    > N수 = 데이터 개수, K = 데이터의 최대값
 
 ## 5. Binary Insertion Sort
 - 이진 삽입 정렬
 
 ## 6. Heap Sort
-- 힙 정
+- 힙 정렬
+#
+###Tip
+Python 정렬 라이브러리
+> sorted( ), sort( )
+>   > 시간 복잡도 : O(NlogN)  
+>  merge sort, insertion sort 아이디어를 더한 하이브리드 방식 정렬 알고리즘.  
+>  key를 매개변수로 입력받아 사용 가능.  
+*  데이터의 개수를 보고 어떤 알고리즘을 쓸지 결정하자.
--- sorting.py
+++ sorting.py
@@ -1,5 +1,5 @@
-def sequential_sort(input_list, reverse):
-
+def sequential_sort(target, reverse):
+    input_list = target.copy()
     if reverse is True:
         for i in range(len(input_list)-1):
             for j in range(i+1,len(input_list)):
@@ -14,7 +14,8 @@ def sequential_sort(input_list, reverse):
     return input_list
 
 
-def insertion_sort(input_list, reverse):
+def insertion_sort(target, reverse):
+    input_list = target.copy()
     if reverse is True:
         for i in range(1, len(input_list)):
             for j in range(i,0,-1):
@@ -33,21 +34,70 @@ def insertion_sort(input_list, reverse):
     return input_list
 
 
-def quick_sort(input_list):
+def quick_sort(input_list, reverse, front, rear):
+    if reverse is False:
+        if front>=rear:
+            return
+        pivot = front
+        left = pivot+1
+        right = rear
+        while left<=right:
+            ## Pivot보다 큰 수를 찾을때 까지.
+            while left<=rear and input_list[left]<=input_list[pivot]:
+                left+=1
+            ## Pivot보다 작은 수를 찾을때 까지.
+            while right>front and input_list[right]>=input_list[pivot]:
+                right-=1
+            if left>right:
+                input_list[right], input_list[pivot] = input_list[pivot], input_list[right];
+            else:
+                input_list[left], input_list[right] = input_list[right], input_list[left]
+        quick_sort(input_list,reverse=False,front=front, rear=right-1)
+        quick_sort(input_list, reverse=False,front=right+1, rear=rear)
+        return input_list
+    else:
+        if front>rear:
+            return
+        pivot = front
+        left = pivot+1
+        right = rear
+        while left<=right:
+            ## Pivot보다 작은 수를 찾을때 까지.
+            while left<=rear and input_list[left]>=input_list[pivot]:
+                left+=1
+            ## Pivot보다  수를 찾을때 까지.
+            while right>front and input_list[right]<=input_list[pivot]:
+                right-=1
+            if left>right:
+                input_list[right], input_list[pivot] = input_list[pivot], input_list[right];
+            else:
+                input_list[left], input_list[right] = input_list[right], input_list[left]
+        quick_sort(input_list,reverse=True,front=front, rear=right-1)
+        quick_sort(input_list, reverse=True,front=right+1, rear=rear)
+        return input_list
+
+
+def binary_insertion_sort(input_list):
     answer=[]
 
     return answer
 
 
-def binary_sort(input_list):
-    answer=[]
-
-    return answer
-
-
-def counting_sort(input_list):
-    answer=[]
-
+def counting_sort(input_list, reverse):
+    answer = list()
+    count_list = [0]*(max(input_list)+1)
+    if reverse is False:
+        for i in range(len(input_list)):
+            count_list[input_list[i]]+=1
+        for i in range(len(count_list)):
+            for j in range(count_list[i]):
+                answer.append(i)
+    else:
+        for i in range(len(input_list)):
+            count_list[input_list[i]]+=1
+        for i in range(len(count_list)-1,0,-1):
+            for j in range(count_list[i]):
+                answer.append(i)
     return answer
 
 
